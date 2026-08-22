9fbd0961097fbef7c6dae92846eeec54b3705256
Rahul Chitkara <rahulchitkara2001@gmail.com>
2026-08-22 11:14:26 +1000

    initialised the stack, program counter, and the CPU data registers

--- main.go
+++ main.go
@@ -6,7 +6,17 @@ import (
 	"github.com/rara-ch/chip-8/internal/memory"
 )
 
+type stack [12]uint16
+type registers [16]byte
+
 func main() {
 	memory := memory.NewMemory()
+	stack := stack{}
+	registers := registers{}
+	pc := 0x200
+
 	fmt.Println("Memory: ", memory)
+	fmt.Println("Stack: ", stack)
+	fmt.Println("Registers: ", registers)
+	fmt.Println("Program Counter: ", pc)
 }
