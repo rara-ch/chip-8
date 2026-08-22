86cda154be09cecd67b9146d2eef0d44cf683158
Rahul Chitkara <rahulchitkara2001@gmail.com>
2026-08-22 16:09:26 +1000

    put all the chip8 components into a struct and implemented parsing the inputed file path and passing it to the memory package

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/rara-ch/chip-8
 
 go 1.25.4
+
+require github.com/stretchr/testify v1.12.1
+
+require go.yaml.in/yaml/v3 v3.0.5 // indirect
--- main.go
+++ main.go
@@ -2,21 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/rara-ch/chip-8/internal/display"
 	"github.com/rara-ch/chip-8/internal/memory"
 )
 
 type stack [12]uint16
 type registers [16]byte
 
-func main() {
-	memory := memory.NewMemory()
-	stack := stack{}
-	registers := registers{}
-	pc := 0x200
+// const pixel = 10
 
-	fmt.Println("Memory: ", memory)
-	fmt.Println("Stack: ", stack)
-	fmt.Println("Registers: ", registers)
-	fmt.Println("Program Counter: ", pc)
+type chip8 struct {
+	memory    memory.Memory
+	display   display.Display
+	stack     stack
+	registers registers
+	pc        int
+}
+
+func main() {
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("please include a path to the .ch8 file you would like to run")
+	}
+
+	file, err := os.ReadFile(args[1])
+	if err != nil {
+		log.Fatalf("could not read file: %v", err)
+	}
+
+	chip8 := &chip8{
+		memory:    memory.NewMemory(file),
+		display:   display.Display{},
+		stack:     stack{},
+		registers: registers{},
+		pc:        0x200,
+	}
+	fmt.Println(chip8)
 }
